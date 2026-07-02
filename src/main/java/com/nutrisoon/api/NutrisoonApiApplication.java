0cc53d2ff93a5344ea53c3d3b7f07c81418111fd
BID_OVARELA <ovarela.bid@gmail.com>
2026-07-01 23:57:32 -0600

    initial commit backend setup

---
+++ src/main/java/com/nutrisoon/api/NutrisoonApiApplication.java
@@ -0,0 +1,13 @@
+package com.nutrisoon.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrisoonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrisoonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutrisoon/api/NutrisoonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrisoon.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrisoonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
